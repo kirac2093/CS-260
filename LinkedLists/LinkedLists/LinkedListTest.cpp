859f85fcd5f6584d7d73921c756e1594d6dc2c6a
kirac2093 <kirac2093@gmail.com>
2018-04-10 22:05:35 -0700

    Student class needs compiler errors fixed

---
+++ LinkedLists/LinkedLists/LinkedListTest.cpp
@@ -0,0 +1,90 @@
+/************************************************
+Name: LinkedListTest
+Code: Kira Corbett
+Date: 4/10/18
+Desc: Tests LinkedLists in C++
+************************************************/
+#include <iostream>
+#include "UnOrderedList.h"
+#include "Student.h"
+
+using namespace std;
+
+int main() 
+{
+
+	// test template with ints --------------------------------
+	UnOrderedList<int> intList;
+	intList.prepend(10);
+	intList.prepend(20);
+	intList.prepend(5);
+	intList.append(15);
+	intList.append(25);
+	intList.print();
+
+	cout << endl;
+
+	cout << "5 is in position " << intList.find(5) << endl;
+	cout << "15 is in position " << intList.find(15) << endl;
+	cout << "20 is in position " << intList.find(20) << endl;
+	cout << "8 is in position " << intList.find(8) << endl;
+
+	intList.remove(15);
+	intList.remove(5);
+	intList.remove(25);
+	intList.print();
+
+	intList.removeAll();
+	intList.print();
+	cout << endl << endl;
+
+	// test on list of chars --------------------------------
+	//UnOrderedList<char>* charList = new UnOrderedList<char>;
+	//charList->append('k');
+	//charList->append('s');
+	//charList->prepend('r');
+	//charList->prepend('o');
+	//charList->prepend('w');
+
+	//charList->remove('r');
+	//charList->remove('w');
+	//charList->remove('s');
+	//charList->print();
+
+	//charList->removeAll();
+	//charList->print();
+	//delete charList;
+
+	//// test it on Students ------------------------------------
+	//Student student1("Jen", 'A');
+	//Student student2("Jack", 'B');
+	//Student student3("Jane", 'C');
+	//Student student4("Jelly", 'F');
+	//Student student5("Jackson", 'D');
+
+	//UnOrderedList<Student> studentList;
+
+	//studentList.append(student1);
+	//studentList.append(student1);
+	//studentList.append(student1);
+	//studentList.print();
+
+	//studentList.remove(student3);
+	//studentList.print();
+
+	//studentList.prepend(student3);
+	//studentList.prepend(student4);
+	//studentList.prepend(student5);
+	//studentList.find(student1);
+	//studentList.print();
+
+	//studentList.removeAll();
+
+	//studentList.print();
+
+	cin.get();
+	cin.ignore();
+
+	return 1;
+}
+
---
+++ LinkedLists/LinkedLists/Student.h
@@ -0,0 +1,36 @@
+/************************************************
+Name: Student.h
+Code: Kira Corbett
+Date: 4/10/18
+Desc: Student class
+************************************************/
+
+#pragma once
+#include <iostream>
+#include <string>
+
+class Student {
+public:
+	//constructor
+	Student(const std::string name = "anonymous", char letterGrade = 'W');
+
+	//operator overloads
+	friend bool operator== (const Student& stu1, const Student& stu2)
+	{
+		if (stu1.name == stu2.name && stu1.letterGrade == stu2.letterGrade)
+			return true;
+		else
+			return false;
+	}
+
+	friend std::ostream& operator<< (std::ostream& stream, const Student& student)
+	{
+		stream << student.name;
+		stream << " (" << student.letterGrade << ") ";
+		return stream;
+	}
+
+private:
+	std::string name;
+	char letterGrade;
+};
